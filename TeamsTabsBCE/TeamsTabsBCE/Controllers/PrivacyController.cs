0c282e69612771312c97909786a95d33c23d2be4
Gábor Gróf <hungabor01@gmail.com>
2023-10-26 19:40:11 +0200

    Create solution and project.

---
+++ TeamsTabsBCE/TeamsTabsBCE/Controllers/PrivacyController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TeamsTabsBCE.Controllers
+{
+    public class PrivacyController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.Message = "Add your privacy statement here...";
+            return View();
+        }
+    }
+}
